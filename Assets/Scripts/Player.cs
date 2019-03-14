d96d0fc700f4ad296dd5da9c276845c512a98291
Alex Kastanek <akastanek@nevada.unr.edu>
2019-03-14 02:40:13 -0700

    Created player object and script

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float balance = 20f;
+    public float wager = 0f;
+
+}

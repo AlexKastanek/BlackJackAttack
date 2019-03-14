625bfd99fddd9ecc754c91f9e37d00e198ade01e
Alex Kastanek <akastanek@nevada.unr.edu>
2019-03-14 00:13:54 -0700

    Initial commit

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Suit
+{
+    HEARTS,
+    DIAMONDS,
+    CLUBS,
+    SPADES
+};
+
+public class Card : MonoBehaviour
+{
+
+    public int rank;
+    public Suit suit;
+
+}

8bc30a7c3d3c0a751e3101c3e9758f5005e22300
Alex Kastanek <akastanek@nevada.unr.edu>
2019-03-14 02:04:04 -0700

    Added singleton and game manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameState
+{
+
+}
+
+public class GameManager : Singleton<GameManager> {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
